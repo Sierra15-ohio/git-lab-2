5f146c8894c8448233ea0cfb6715a3b3012638c1
Sierra15-ohio <se494823@ohio.edu>
2026-09-16 12:03:09 -0400

    DONE

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Sierra Evans
+ *        Date: 09/01/2026
+ * Description: Creating a C++ program in git-lab2
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+    cout << "Hello Git!!" << endl;
+    /*add code*/
+    return 0;
+}// main
